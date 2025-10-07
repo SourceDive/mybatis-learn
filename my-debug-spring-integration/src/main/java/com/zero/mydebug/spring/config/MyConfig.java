b54d50b3cd36f08e90fe67daf40ba42fad32da9f
ongoing-z <code@ongoingblog.org>
2025-10-07 20:32:01 +0800

    spring-integration: 提取配置至 MyConfig，精简启动类；补充 Lombok 依赖与 import

---
+++ my-debug-spring-integration/src/main/java/com/zero/mydebug/spring/config/MyConfig.java
@@ -0,0 +1,50 @@
+package com.zero.mydebug.spring.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.zero.mydebug.spring.dao")
+@EnableTransactionManagement
+public class MyConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("testdb")
+                .addScript("classpath:schema.sql")
+                .build();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        // 设置实体类别名包
+        sessionFactory.setTypeAliasesPackage("com.zero.mydebug.spring.domain");
+
+        // 配置 MyBatis 设置
+        org.apache.ibatis.session.Configuration configuration = new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setCacheEnabled(true);
+        configuration.setLazyLoadingEnabled(true);
+        configuration.setAggressiveLazyLoading(false);
+        configuration.setDefaultExecutorType(org.apache.ibatis.session.ExecutorType.REUSE);
+        configuration.setDefaultStatementTimeout(25);
+        configuration.setLogImpl(org.apache.ibatis.logging.stdout.StdOutImpl.class);
+
+        sessionFactory.setConfiguration(configuration);
+
+        return sessionFactory.getObject();
+    }
+}
